ac1ce51bb862b4da91f6861d75e76c56d3ace801
Holden Nelson <holden@Holdens-MacBook-Pro.local>
2026-08-22 18:26:46 -0600

    initial commit, basic loop and error handling

---
+++ src/lox.rs
@@ -0,0 +1,73 @@
+use std::{fs, io::{self, BufRead, Write}};
+use thiserror::Error;
+
+pub struct Lox { }
+
+impl Lox {
+    pub fn new() -> Self {
+        Self { }
+    }
+
+    pub fn run_file(&self, path: &str) -> Result<(), LoxError> {
+        let source = fs::read_to_string(path)?;
+        Self::run(&source)?;
+        Ok(())
+    }
+
+    pub fn run_prompt(&self) -> Result<(), LoxError> {
+        let stdin = io::stdin();
+        let mut lines = stdin.lock().lines();
+
+        let stdout = io::stdout();
+        let mut stdout = stdout.lock();
+
+        loop {
+            write!(stdout, "> ")?;
+            stdout.flush()?;
+
+            let Some(line) = lines.next() else {
+                break;
+            };
+
+            let line = line?;
+            Self::run(&line)?;
+        }
+
+        Ok(())
+    }
+
+    fn run(source: &str) -> Result<(), LoxError> {
+        println!("{source}");
+        Ok(())
+    }
+
+
+}
+
+#[derive(Debug, Error)]
+#[error("[line {line}] Error {at}: {message}")]
+pub struct CompileError {
+    pub line: usize,
+    pub at: String,
+    pub message: String
+}
+
+#[derive(Debug, Error)]
+#[error("[line {line}] Error {at}: {message}")]
+pub struct RunTimeError {
+    pub line: usize,
+    pub at: String,
+    pub message: String
+}
+
+#[derive(Debug, Error)]
+pub enum LoxError {
+    #[error("I/O error: {0}")]
+    Io(#[from] io::Error),
+
+    #[error(transparent)]
+    Compile(CompileError),
+
+    #[error(transparent)]
+    RunTime(RunTimeError)
+}
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+mod lox;
+mod tokens;
+
+use std::{env, process::ExitCode};
+
+use crate::lox::Lox;
+
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    let lox = Lox::new();
+
+    let result = match args.as_slice() {
+        [] => lox.run_prompt(),
+        [script] => lox.run_file(script),
+        _ => {
+            eprintln!("Usage: rlox [script]");
+            return ExitCode::FAILURE;
+        }
+    };
+
+    match result {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(error) => {
+            eprintln!("{error}");
+            ExitCode::FAILURE
+        }
+    }
+}
